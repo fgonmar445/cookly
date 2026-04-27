ef13e74f4b09be6ba1899de193a4c6124a2d501c
fgonmar445 <fgonmar445@g.educaand.es>
2026-04-27 17:56:29 +0200

    Mejora UX

--- resources/views/recetas/show.blade.php
+++ resources/views/recetas/show.blade.php
@@ -4,8 +4,7 @@
 
 {{-- BOTÓN VOLVER --}}
 <div class="flex justify-end">
-    <a href="{{ url()->previous() }}"
-        class="flex items-center gap-1 text-blue-600 hover:text-blue-800 mb-4">
+    <a href="{{ route('buscar') }}" class="flex items-center gap-1 text-blue-600 hover:text-blue-800 mb-4">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
         </svg>
