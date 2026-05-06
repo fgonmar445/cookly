8e5c87bafc6463b5c78dd201142f63853b3a1933
fgonmar445 <fgonmar445@g.educaand.es>
2026-05-06 11:35:46 +0200

    Seeder config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Ejecutar el seeder de administradores y usuarios de prueba
+        $this->call(AdminSeeder::class);
     }
 }
