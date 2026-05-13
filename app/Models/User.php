9ec9ab1523344ace3a4d1d1961a8106f7dfceb91
fgonmar445 <fgonmar445@g.educaand.es>
2026-05-13 11:19:34 +0200

    Verify Email

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Ingrediente;
 use App\Models\Favorito;
 use App\Models\ListaIngrediente;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
