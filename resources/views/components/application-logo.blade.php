a366f8e5337243deab47671380a907090666f895
fgonmar445 <fgonmar445@g.educaand.es>
2026-05-15 12:54:16 +0200

    Logo update

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <div {{ $attributes->merge(['class' => 'flex items-center justify-center']) }}>
-    <img src="{{ asset('logo-cookly.svg') }}" alt="Cookly Logo" class="w-full h-full object-contain">
+    <img src="{{ asset('cookly_logo.png') }}" alt="Cookly Logo" class="w-full h-full object-contain">
 </div>
